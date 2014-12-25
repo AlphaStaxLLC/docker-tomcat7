5e07d2f85eabcdc03d6f67ecf412e0fdd297d7fe
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-24 21:19:33 -0500

    Create pre-conf.sh

---
+++ pre-conf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+/usr/bin/mysqld_safe &
+
+sleep 5s
+
+mysqladmin -u root password mysqlpsswd
+mysqladmin -u root -pmysqlpsswd reload
+mysqladmin -u root -pmysqlpsswd create tomcat7
+
+echo "GRANT ALL ON drupal.* TO tomcat7user@localhost IDENTIFIED BY 'tomcat7dbpasswd'; flush privileges; " | mysql -u root -pmysqlpsswd
+
+#conf tomcat7 for dspace
+#a=$(cat /etc/tomcat7/server.xml | grep -n "</Host>"| cut -d : -f 1 )
+#sed -i "$((a-1))r /tmp/dspace_tomcat7.conf" /etc/tomcat7/server.xml
+
+killall mysqld
+sleep 5s
