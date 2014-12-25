eac0cab2a61b0b2027096fc2209479df82ea4d3c
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-24 23:58:58 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-MYSQL_PASSWD=${MYSQL_PASSWD:-mysqlpsswd}
-/usr/bin/mysqld_safe &
-
-sleep 5s
-
-mysqladmin -u root password ${MYSQL_PASSWD}
-mysqladmin -u root -p${MYSQL_PASSWD} reload
 
 chgrp -R tomcat7 /etc/tomcat7
 chmod -R g+w /etc/tomcat7 
-
-killall mysqld
-sleep 5s
