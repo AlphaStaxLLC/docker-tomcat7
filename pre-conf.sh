54835c2cc13ad118fa2f2a5aa9e7e3dfca3e637b
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-24 22:37:16 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -5,13 +5,7 @@ sleep 5s
 
 mysqladmin -u root password mysqlpsswd
 mysqladmin -u root -pmysqlpsswd reload
-mysqladmin -u root -pmysqlpsswd create tomcat7
 
-echo "GRANT ALL ON tomcat7.* TO tomcat7user@localhost IDENTIFIED BY 'tomcat7dbpasswd'; flush privileges; " | mysql -u root -pmysqlpsswd
-
-#conf tomcat7 for dspace
-#a=$(cat /etc/tomcat7/server.xml | grep -n "</Host>"| cut -d : -f 1 )
-#sed -i "$((a-1))r /tmp/dspace_tomcat7.conf" /etc/tomcat7/server.xml
 chgrp -R tomcat7 /etc/tomcat7
 chmod -R g+w /etc/tomcat7 
 
